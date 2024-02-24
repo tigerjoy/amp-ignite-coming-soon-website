2df98ce754090589f2b59025d0f2ca9752cc4476
tigerjoy <ranajoy123saha@gmail.com>
2024-02-24 17:51:32 +0530

    Added a view to save contact details

--- coming_soon_website/views.py
+++ coming_soon_website/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def submit_contact_details(request):
+    if request.method == 'POST':
+        # Get the timezone from the IP address
+        ip_address = request.META.get('REMOTE_ADDR')
+
+        return JsonResponse({'ip_address': ip_address, 'message': 'success'})
+    else:
+        return JsonResponse({'message': 'Method not allowed'}, status=405)
