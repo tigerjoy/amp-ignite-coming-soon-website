a6b44952d4f5b877aa9899d9cf8b5beda0fa87c3
tigerjoy <ranajoy123saha@gmail.com>
2024-02-26 23:53:54 +0530

    Made settings.py more generic

--- ampignite_coming_soon_website/settings.py
+++ ampignite_coming_soon_website/settings.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 
 from pathlib import Path
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,28 +24,31 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vlpdlil9^n5_no^7&d4ib4_l@iz@h&gm@m@z%nejc#j73ik(#x'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-load_dotenv()
+ALLOWED_HOSTS = str(os.getenv("ALLOWED_HOSTS")).split(",")
 
 # Load all constants
 CLIENT_ID = os.getenv("GO_HIGH_LEVEL_CLIENT_ID")
 CLIENT_SECRET = os.getenv("GO_HIGH_LEVEL_CLIENT_SECRET")
 TOKEN_REFRESH_THRESHOLD_MINUTES = int(os.getenv("TOKEN_REFRESH_THRESHOLD_MINUTES"))
-EMAIL_TO=str(os.getenv("EMAIL_TO")).split(",")
-GO_HIGH_LEVEL_SERVICE_NAME=os.getenv("GO_HIGH_LEVEL_SERVICE_NAME")
-CRON_JOBS_LOG_FILE_PATH=os.getenv("CRON_JOBS_LOG_FILE_PATH")
+EMAIL_TO = str(os.getenv("EMAIL_TO")).split(",")
+GO_HIGH_LEVEL_SERVICE_NAME = os.getenv("GO_HIGH_LEVEL_SERVICE_NAME")
+CRON_JOBS_LOG_FILE_PATH = os.getenv("CRON_JOBS_LOG_FILE_PATH")
+
+# Database Constants
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 
 # django-crontab definitions
 CRONTAB_LOCK_JOBS = True
 
 CRONJOBS = [
-    ("*/1 * * * *", 'django.core.management.call_command', ['process_tasks'])
+    ("* */1 * * *", 'django.core.management.call_command', ['process_tasks'])
 ]
 
 CRONTAB_COMMAND_SUFFIX = f'>> "{CRON_JOBS_LOG_FILE_PATH}" 2>&1'
@@ -98,12 +103,24 @@ WSGI_APPLICATION = 'ampignite_coming_soon_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': DATABASE_NAME,
+            'USER': DATABASE_USERNAME,
+            'PASSWORD': DATABASE_PASSWORD,
+            'HOST': 'localhost',
+            'PORT': ''
+        }
+    }
 
 
 # Password validation
@@ -140,7 +157,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
