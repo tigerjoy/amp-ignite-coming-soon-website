8336d0aed78a9fb165f65c316805ff9b8dc01ff3
Ranajoy Saha <ranajoy123saha@gmail.com>
2024-02-27 13:17:28 +0000

    Added submit-notification-details view

--- coming_soon_website/views.py
+++ coming_soon_website/views.py
@@ -2,13 +2,42 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.conf import settings
 
-from .models import ContactFormData
+from .models import ContactFormData, NotificationFormData
 
 from .tasks.go_high_level_tasks import save_contact_flow
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def submit_notification_details(request):
+    required_fields = ["email"]
+    required_fields_exist = all([attribute in request.POST for attribute in required_fields])
+
+    if request.method == 'POST' and required_fields_exist:
+        print(request.POST)
+        # Get the timezone from the IP address
+        ip_address = request.META.get('REMOTE_ADDR')
+
+        # Get timezone
+        timezone = request.session.get('django_timezone', 'UTC')
+
+        form_data = {
+            "email": request.POST["email"]
+        }
+
+        notification_form_data = NotificationFormData(**form_data)
+        
+        # Save notification to DB
+        notification_form_data.save()
+
+        form_data['message'] = 'success'
+
+        return JsonResponse(form_data)
+    elif request.method == 'GET':
+        return JsonResponse({'message': 'Method not allowed'}, status=405)
+    else:
+        return JsonResponse({'message': 'Invalid request'}, status=400)
 
 def submit_contact_details(request):
     required_fields = ["website_type", "form_type", "name", "company", "email", "phone", "customer_message"]
